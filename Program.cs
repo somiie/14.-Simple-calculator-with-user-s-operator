7a23a0d1f2814d52cf9a26a0c9838b5b7f1f1c3e
Somie1 <somelezesipayile@gmail.com>
2024-01-15 10:25:59 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.Simple_calculator_with_user_s_operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an operator ");
+            string op = Console.ReadLine();
+
+            Console.WriteLine("Enter second number ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+'")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else
+            {
+                Console.WriteLine("Not an operator");
+            }
+
+            Console.Read();
+        }
+    }
+}
